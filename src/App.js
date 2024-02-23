42c7f319ab8b381519a4f4c6b3bfabeb2a96218d
IAMOTAS <niiotasalokome@gmail.com>
2024-02-23 01:10:02 +0000

    fixes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-
 import './App.css';
+import Header from './components/Header'; 
+import Balance from './components/Balance';
+import IncomeExpense from './components/IncomeExpense';
+import TransactionList from './components/TransactionList';
+import AddTransactions from './components/AddTransactions';
+import { GlobalProvider } from './context/GlobalState';
+
 
 function App() {
   return (
-    <div className="App">
-     otas
+
+    
+
+<GlobalProvider>
+  <Header/>
+    <div className='container'>
+<Balance/>
+<IncomeExpense/>
+<TransactionList/>
+<AddTransactions/>
     </div>
+    </GlobalProvider>
+
+  
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+
+      <h1>
+       <em> Track Your Expenditure</em>
+        </h1>
+    
+
+
+  ) 
+}
+
+export default Header;
